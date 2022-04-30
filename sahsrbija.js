7251f9b13580ecf4561593b71ab2f77646615d31
DavidBugarski <101926862+DavidBugarski@users.noreply.github.com>
2022-04-30 13:48:41 +0200

    Add files via upload

---
+++ sahsrbija.js
@@ -0,0 +1,41 @@
+<script type="text/javascript">
+
+var loginName = "";
+var userid = _spPageContextInfo.userId;
+GetCurrentUser();
+
+function GetCurrentUser() {
+var requestUri = _spPageContextInfo.webAbsoluteUrl + "/_api/web/getuserbyid(" + userid + ")";
+
+var requestHeaders = { "accept" : "application/json;odata=verbose" };
+
+$.ajax({
+  url : requestUri,
+  contentType : "application/json;odata=verbose",
+  headers : requestHeaders,
+  success : onSuccess,
+  error : onError
+  });
+}
+
+function onSuccess(data, request) {
+    var loginName = data.d.Title;
+    alert("Hello " + loginName);
+
+    // to set the "hello username" into the page
+    document.getElementById("id").innerHTML = "Hello " + loginName;
+    }
+
+function onError(error) {
+  alert(error);
+  }
+
+</script>
+<div id="current_date"></p>
+<script>
+date = new Date();
+year = date.getFullYear();
+month = date.getMonth() + 1;
+day = date.getDate();
+document.getElementById("current_date").innerHTML = month + "/" + day + "/" + year;
+</script>
