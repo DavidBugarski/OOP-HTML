352e3c8c6e45bd1c444ded0c36de36b997064786
DavidBugarski <101926862+DavidBugarski@users.noreply.github.com>
2022-06-03 17:38:45 +0200

    Add files via upload

--- sahsrbija.js
+++ sahsrbija.js
@@ -1,45 +1,19 @@
-<script type="text/javascript">
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-var loginName = "";
-var userid = _spPageContextInfo.userId;
-GetCurrentUser();
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-function GetCurrentUser() {
-var requestUri = _spPageContextInfo.webAbsoluteUrl + "/_api/web/getuserbyid(" + userid + ")";
-
-var requestHeaders = { "accept" : "application/json;odata=verbose" };
-
-$.ajax({
-  url : requestUri,
-  contentType : "application/json;odata=verbose",
-  headers : requestHeaders,
-  success : onSuccess,
-  error : onError
-  });
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-function onSuccess(data, request) {
-    var loginName = data.d.Title;
-    alert("Hello " + loginName);
-
-    // to set the "hello username" into the page
-    document.getElementById("id").innerHTML = "Hello " + loginName;
-    }
-
-function onError(error) {
-  alert(error);
-  }
-
-</script>
-<div id="current_date"></p>
-<script>
-date = new Date();
-year = date.getFullYear();
-month = date.getMonth() + 1;
-day = date.getDate();
-document.getElementById("current_date").innerHTML = month + "/" + day + "/" + year;
-</script>
-<script>
-            var dt = new Date();
-            document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
-</script>
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
